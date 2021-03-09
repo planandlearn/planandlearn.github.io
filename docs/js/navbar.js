0aa37046d517b1af601e91e10dab890802b78115
Lee Yiyuan <leeyiyuan@hotmail.com>
2021-03-09 19:59:30 +0800

    Delete logging

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -3,9 +3,6 @@
     var top_of_element = $("#nav").offset().top;
     var top_of_screen = $(window).scrollTop();
 
-    console.log(top_of_element);
-    console.log(top_of_screen);
-
     if (top_of_element < top_of_screen) {
       $("#fixed-nav").css({display: 'block'});
     } else {
