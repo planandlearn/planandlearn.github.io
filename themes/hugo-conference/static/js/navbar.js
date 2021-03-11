08c9a0bcc151cfe20ed10d601fb270a301532b5b
Lee Yiyuan <leeyiyuan@hotmail.com>
2021-03-11 10:51:29 +0800

    Update GilWoo's info

--- themes/hugo-conference/static/js/navbar.js
+++ themes/hugo-conference/static/js/navbar.js
@@ -1,12 +1,17 @@
+update_nav = function() {
+  var top_of_element = $("#nav").offset().top;
+  var top_of_screen = $(window).scrollTop();
+
+  if (top_of_element < top_of_screen) {
+    $("#fixed-nav").css({display: 'block'});
+  } else {
+    $("#fixed-nav").css({display: 'none'});
+  }
+};
+
 (function() {
   $(window).scroll(function() {
-    var top_of_element = $("#nav").offset().top;
-    var top_of_screen = $(window).scrollTop();
-
-    if (top_of_element < top_of_screen) {
-      $("#fixed-nav").css({display: 'block'});
-    } else {
-      $("#fixed-nav").css({display: 'none'});
-    }
+    update_nav();
   });
 }());
+update_nav();
